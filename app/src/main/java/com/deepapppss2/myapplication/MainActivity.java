f6f68d8e2ca297e1961e50eede753f17a89f5f26
DeepPriyesh <priyeshdeep535@gmail.com>
2021-08-06 01:18:39 +0530

    Notes App

---
+++ app/src/main/java/com/deepapppss2/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.deepapppss2.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitData(View view) {
+        Toast.makeText(this,"Insert Data",Toast.LENGTH_LONG).show();
+    }
+}
